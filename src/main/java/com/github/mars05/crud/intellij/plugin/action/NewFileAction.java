fe97de5bbe23e95c0b87e0d9d9870aa2b9e6c217
xiaoyu <xiaoyucoder@163.com>
2019-09-24 18:00:41 +0800

    移除无用代码

--- src/main/java/com/github/mars05/crud/intellij/plugin/action/NewFileAction.java
+++ src/main/java/com/github/mars05/crud/intellij/plugin/action/NewFileAction.java
@@ -18,57 +18,54 @@ import com.intellij.openapi.roots.ModuleRootManager;
 import com.intellij.openapi.vfs.VirtualFile;
 import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.idea.maven.project.MavenProjectsManager;
 
 /**
  * @author xiaoyu
  */
 public class NewFileAction extends AnAction {
-    @Override
-    public void actionPerformed(AnActionEvent e) {
-        Project project = e.getProject();
-        VirtualFile virtualFile = e.getData(DataKeys.VIRTUAL_FILE);
-        if (!virtualFile.isDirectory()) {
-            virtualFile = virtualFile.getParent();
-        }
-        Module module = ModuleUtil.findModuleForFile(virtualFile, project);
+	@Override
+	public void actionPerformed(AnActionEvent e) {
+		Project project = e.getProject();
+		VirtualFile virtualFile = e.getData(DataKeys.VIRTUAL_FILE);
+		if (!virtualFile.isDirectory()) {
+			virtualFile = virtualFile.getParent();
+		}
+		Module module = ModuleUtil.findModuleForFile(virtualFile, project);
 
-        String moduleRootPath = ModuleRootManager.getInstance(module).getContentRoots()[0].getPath();
-        String actionDir = virtualFile.getPath();
+		String moduleRootPath = ModuleRootManager.getInstance(module).getContentRoots()[0].getPath();
+		String actionDir = virtualFile.getPath();
 
-        String str = StringUtils.substringAfter(actionDir, moduleRootPath + "/src/main/java/");
-        String basePackage = StringUtils.replace(str, "/", ".");
-        SelectionContext.clearAllSet();
+		String str = StringUtils.substringAfter(actionDir, moduleRootPath + "/src/main/java/");
+		String basePackage = StringUtils.replace(str, "/", ".");
+		SelectionContext.clearAllSet();
 
-        SelectionContext.setPackage(basePackage);
-        if (StringUtils.isNotBlank(basePackage)) {
-            basePackage += ".";
-        }
-        SelectionContext.setControllerPackage(basePackage + "controller");
-        SelectionContext.setServicePackage(basePackage + "service");
-        SelectionContext.setDaoPackage(basePackage + "dao");
-        SelectionContext.setModelPackage(basePackage + "model");
-        SelectionContext.setMapperDir(moduleRootPath + "/src/main/resources/mapper");
+		SelectionContext.setPackage(basePackage);
+		if (StringUtils.isNotBlank(basePackage)) {
+			basePackage += ".";
+		}
+		SelectionContext.setControllerPackage(basePackage + "controller");
+		SelectionContext.setServicePackage(basePackage + "service");
+		SelectionContext.setDaoPackage(basePackage + "dao");
+		SelectionContext.setModelPackage(basePackage + "model");
+		SelectionContext.setMapperDir(moduleRootPath + "/src/main/resources/mapper");
 
-        CrudActionDialog dialog = new CrudActionDialog(project, module);
-        if (!dialog.showAndGet()) {
-            return;
-        }
-        DumbService.getInstance(project).runWhenSmart((DumbAwareRunnable) () -> new WriteCommandAction(project) {
-            @Override
-            protected void run(@NotNull Result result) {
-                Selection selection = SelectionContext.copyToSelection();
-                SelectionContext.clearAllSet();
-                try {
-                    PsiFileUtils.createCrud(project, selection, moduleRootPath);
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
-                //解决依赖
-                MavenProjectsManager.getInstance(project).forceUpdateAllProjectsOrFindAllAvailablePomFiles();
-                //优化生成的所有Java类
-                CrudUtils.doOptimize(project);
-            }
-        }.execute());
-    }
+		CrudActionDialog dialog = new CrudActionDialog(project, module);
+		if (!dialog.showAndGet()) {
+			return;
+		}
+		DumbService.getInstance(project).runWhenSmart((DumbAwareRunnable) () -> new WriteCommandAction(project) {
+			@Override
+			protected void run(@NotNull Result result) {
+				Selection selection = SelectionContext.copyToSelection();
+				SelectionContext.clearAllSet();
+				try {
+					PsiFileUtils.createCrud(project, selection, moduleRootPath);
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+				//优化生成的所有Java类
+				CrudUtils.doOptimize(project);
+			}
+		}.execute());
+	}
 }
