d60819a69ed71651687e747a3d3892c20d42e7b1
xiaoyu <xiaoyucoder@163.com>
2019-09-25 09:46:32 +0800

    设置模板编码utf-8

--- src/main/java/com/github/mars05/crud/intellij/plugin/util/FreemarkerConfiguration.java
+++ src/main/java/com/github/mars05/crud/intellij/plugin/util/FreemarkerConfiguration.java
@@ -3,14 +3,15 @@ package com.github.mars05.crud.intellij.plugin.util;
 import freemarker.template.Configuration;
 
 public class FreemarkerConfiguration extends Configuration {
-    private String basePackagePath;
+	private String basePackagePath;
 
-    public FreemarkerConfiguration() {
-        this("");
-    }
+	public FreemarkerConfiguration() {
+		this("");
+	}
 
-    public FreemarkerConfiguration(String basePackagePath) {
-        super(Configuration.DEFAULT_INCOMPATIBLE_IMPROVEMENTS);
-        setClassForTemplateLoading(getClass(), basePackagePath);
-    }
+	public FreemarkerConfiguration(String basePackagePath) {
+		super(Configuration.DEFAULT_INCOMPATIBLE_IMPROVEMENTS);
+		setDefaultEncoding("UTF-8");
+		setClassForTemplateLoading(getClass(), basePackagePath);
+	}
 }
