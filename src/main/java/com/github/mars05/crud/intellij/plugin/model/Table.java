d2bc25118db80f358af2b3f86dd510de9beae03d
xiaoyu <xiaoyucoder@163.com>
2022-03-16 16:47:27 +0800

    table调整

--- src/main/java/com/github/mars05/crud/intellij/plugin/model/Table.java
+++ src/main/java/com/github/mars05/crud/intellij/plugin/model/Table.java
@@ -4,11 +4,8 @@ package com.github.mars05.crud.intellij.plugin.model;
 import com.google.common.base.CaseFormat;
 import lombok.Getter;
 import lombok.Setter;
-import org.apache.commons.lang3.StringUtils;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 /**
  * @author xiaoyu
@@ -21,7 +18,6 @@ public class Table {
     private String upperCamelName;
     private String remarks;
     private List<Column> columns;
-    private Set<String> imports;
 
     public String getLowerCamelName() {
         return CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, tableName);
@@ -31,13 +27,4 @@ public class Table {
         return CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, tableName);
     }
 
-    public Set<String> getImports() {
-        imports = new HashSet<>();
-        for (Column column : columns) {
-            if (!column.getJavaTypeClass().isPrimitive() && !"java.lang".equals(StringUtils.substringBeforeLast(column.getJavaTypeClass().getName(), "."))) {
-                imports.add(column.getJavaTypeClass().getName());
-            }
-        }
-        return imports;
-    }
 }
