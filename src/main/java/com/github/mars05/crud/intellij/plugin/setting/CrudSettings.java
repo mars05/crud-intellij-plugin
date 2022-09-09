06096332e5d08a7063f6061967656ec6e2f51b7d
xiaoyu <xiaoyucoder@163.com>
2022-09-09 11:21:13 +0800

    加载默认模板异常处理

--- src/main/java/com/github/mars05/crud/intellij/plugin/setting/CrudSettings.java
+++ src/main/java/com/github/mars05/crud/intellij/plugin/setting/CrudSettings.java
@@ -6,6 +6,7 @@ import com.github.mars05.crud.hub.common.util.BeanUtils;
 import com.github.mars05.crud.intellij.plugin.dao.model.DataSourceDO;
 import com.github.mars05.crud.intellij.plugin.dao.model.ProjectTemplateDO;
 import com.github.mars05.crud.intellij.plugin.dto.GenerateDTO;
+import com.github.mars05.crud.intellij.plugin.util.CrudUtils;
 import com.intellij.openapi.components.PersistentStateComponent;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.components.State;
@@ -14,7 +15,6 @@ import com.intellij.openapi.util.io.StreamUtil;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.InputStream;
-import java.nio.charset.Charset;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -46,8 +46,11 @@ public class CrudSettings implements PersistentStateComponent<CrudState> {
     public static List<ProjectTemplateDO> getProjectTemplates() {
         if (!CRUD_SETTINGS.myState.isInitialized()) {
             try {
-                InputStream is = CRUD_SETTINGS.myState.getClass().getResourceAsStream("/templates/default_pts.json");
-                String readText = StreamUtil.readText(is, Charset.defaultCharset());
+                InputStream is = CrudSettings.class.getResourceAsStream("/templates/default_pts.json");
+                if (is == null) {
+                    throw new NullPointerException("默认模板加载失败");
+                }
+                String readText = StreamUtil.readText(is, CrudUtils.UTF_8);
                 CRUD_SETTINGS.myState.getProjectTemplates().clear();
                 List<ProjectTemplateDTO> list = JSON.parseArray(readText, ProjectTemplateDTO.class);
                 CRUD_SETTINGS.myState.getProjectTemplates().addAll(list.stream().map(projectTemplateDTO -> {
@@ -55,7 +58,8 @@ public class CrudSettings implements PersistentStateComponent<CrudState> {
                     projectTemplateDO.setFileTemplates(JSON.toJSONString(projectTemplateDTO.getFileTemplateList()));
                     return projectTemplateDO;
                 }).collect(Collectors.toList()));
-            } catch (Exception ignored) {
+            } catch (Exception e) {
+                e.printStackTrace();
             } finally {
                 CRUD_SETTINGS.myState.setInitialized(true);
             }
